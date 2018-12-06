2fad7926a76d24b898bf9dc9c2e6cb2657af75f3
batman <buddies2705@gmail.com>
2018-12-07 02:36:37 +0530

    changes

--- console.js
+++ console.js
@@ -2,7 +2,7 @@ var repl = require("repl");
 var db = require("./server/models/index.js");
 
 var replServer = repl.start({
-  prompt: "Sequelize | mysql | Mysql > ",
+  prompt: "Sequelize | PostgreSQL > ",
 });
 
 replServer.context.db = db;
