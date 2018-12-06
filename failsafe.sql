5b505718e6c2e3bcdc442264a451d032d00d4ade
batman <buddies2705@gmail.com>
2018-12-07 03:14:46 +0530

    changes

--- failsafe.sql
+++ failsafe.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS cryptofolio;
-CREATE DATABASE cryptofolio;
-DROP DATABASE IF EXISTS cryptofolio_test;
-CREATE DATABASE cryptofolio_test;
+DROP DATABASE IF EXISTS d8hjahsmge6jui;
+CREATE DATABASE d8hjahsmge6jui;
+DROP DATABASE IF EXISTS d8hjahsmge6jui_test;
+CREATE DATABASE d8hjahsmge6jui_test;
